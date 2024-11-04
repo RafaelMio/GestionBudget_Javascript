51ffebd4615c7dcab7ff527e509c95a864c7d6ee
Rafouki <58076280+Rafouki@users.noreply.github.com>
2024-11-04 18:12:49 +0100

    Transaction functions

---
+++ script.js
@@ -0,0 +1,44 @@
+const revenueNameInput = document.getElementById('addRevenueName');
+const revenueNbInput = document.getElementById('addRevenueNb');
+const expenseNameInput = document.getElementById('addExpenseName');
+const expenseNbInput = document.getElementById('addExpenseNb');
+const addRevenueButton = document.getElementById('addRevenueButton');
+const addExpenseButton = document.getElementById('addExpenseButton');
+const revenueList = document.getElementById('revenueList');
+const expenseList = document.getElementById('expenseList');
+
+function addRevenue() {
+    const text = revenueNameInput.value;
+    const nb = revenueNbInput.value;
+    if (text && nb) {
+        createOperationDiv(text, nb, "revenue");
+        revenueNameInput.value = '';
+        revenueNbInput.value = '';
+    }
+}
+
+function addExpense() {
+    const text = expenseNameInput.value;
+    const nb = expenseNbInput.value;
+    if (text && nb) {
+        createOperationDiv(text, nb, "expense");
+        expenseNameInput.value = '';
+        expenseNbInput.value = '';
+    }
+}
+
+function createOperationDiv(name, number, type) {
+    const productDiv = document.createElement('div');
+    if (type === "revenue") {
+        productDiv.classList.add('revenue');
+        revenueList.appendChild(productDiv);
+    }
+    else {
+        productDiv.classList.add('expense');
+        expenseList.appendChild(productDiv);
+    }
+    productDiv.textContent = `${type} : ${name} - Montant : ${number} €`;
+}
+
+addRevenueButton.addEventListener('click', addRevenue);
+addExpenseButton.addEventListener('click', addExpense);
